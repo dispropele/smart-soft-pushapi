afedccf2aadd6754445464c8414797779d34fd36
dispropele <dispropele@gmail.com>
2026-05-12 02:42:28 +0300

    fixxxx

--- public/index.php
+++ public/index.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+
+    $trustedProxies = $context['TRUSTED_PROXIES'] ?? '127.0.0.1,10.0.0.0/8';
+    if ($trustedProxies) {
+        Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
+    }
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };

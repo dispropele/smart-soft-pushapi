9b49d3e3fab8e2f128958413cb1626b9ad114697
dispropele <dispropele@gmail.com>
2026-05-12 03:49:32 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
 
-    Request::setTrustedProxies(['0.0.0.0/0'], Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
+    Request::setTrustedProxies(['127.0.0.1', '10.0.0.0/8'], Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
     
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
